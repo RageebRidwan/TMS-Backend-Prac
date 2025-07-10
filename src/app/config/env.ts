e18db0894047653d29c25d1c6ecb015ca69c27ec
RageebRidwan <rageeb.cs@gmail.com>
2025-07-11 02:22:28 +0600

    Setup completed

---
+++ src/app/config/env.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+
+dotenv.config(); // loads my .env into the process.env
+interface IEnvConfig {
+  PORT: string;
+  DB_URL: string;
+  NODE_ENV: "development" | "production";
+}
+const loadEnvVariables = (): IEnvConfig => {
+  const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV"];
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing required env var: ${key}`);
+    }
+  });
+  return {
+    PORT: process.env.PORT as string,
+    DB_URL: process.env.DB_URL as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+  };
+};
+export const envVars = loadEnvVariables();
---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import { Server } from "http";
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+let server: Server;
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.DB_URL);
+    console.log(`Connected to ${envVars.PORT}`);
+    server = app.listen(envVars.PORT, () => {
+      console.log(`App is listening to ${envVars.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+startServer();
+process.on("unhandledRejection", () => {
+  console.log("Unhandled rejection. server shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    return;
+  }
+  process.exit(1);
+});
+process.on("uncaughtException", () => {
+  console.log("Uncaught exception detected");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    return;
+  }
+  process.exit(1);
+});
+process.on("SIGTERM", () => {
+  console.log("Signal terminated from your deploy server");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+    return;
+  }
+  process.exit(1);
+});
